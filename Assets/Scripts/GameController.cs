8a16a06f63aabb7ac23204ffae43b420594d92e4
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-11-20 04:20:32 -0200

    aumento de velocidade com progressao de tempo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,10 @@ public class GameController : MonoBehaviour {
 
 	public bool gameOver = false;               
 	public float scrollSpeed = -4f;
+	public float speedIncrease = -0.5f;
+	public float periodTime = 0.5f;
+	public float currentTime = 0f;
+	public float speedLimit = -15f;
 
 	void Awake()
 	{
@@ -20,11 +24,23 @@ public class GameController : MonoBehaviour {
 
 	void Update()
 	{
+		AddTime (Time.deltaTime);
+
 		//fim de jogo e alguma tecla pressioanda
 		//resetando cena
 		if (gameOver && Input.GetKeyDown(KeyCode.Space)) 
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		
+	}
+
+	void AddTime(float delta)
+	{
+		currentTime += delta;
+		if (currentTime >= periodTime && speedLimit > scrollSpeed) 
+		{
+			scrollSpeed += speedIncrease;
+			currentTime = 0;	
+		}
 	}
 
 	public void PlayerDied()
