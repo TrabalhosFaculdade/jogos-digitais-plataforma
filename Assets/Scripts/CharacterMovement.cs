cd7b6e1e5edc5a232390f63401cadb08b2a7ab64
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-11-20 00:23:54 -0200

    conceito inicial

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed;
+	public float jumpForce;
+	private Rigidbody2D rb;
+
+	private bool isGrounded, hasDoubleJump;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		isGrounded = true;
+		hasDoubleJump = true;
+	}
+
+	void Update() 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			if (isGrounded || hasDoubleJump) 
+			{
+				rb.AddForce (new Vector2 (0, jumpForce));
+				if (hasDoubleJump && !isGrounded)
+					hasDoubleJump = false;
+			}
+
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Floor") 
+		{
+			isGrounded = true;
+			hasDoubleJump = true;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Floor") 
+			isGrounded = false;
+	}
+}
---
+++ Assets/Scripts/ObstaclesPooling.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesPooling : MonoBehaviour {
+
+	public int columnPoolSize = 5;
+	public GameObject[] columnPrefab;
+	public float spawnRateMax = 5f;
+	public float spawnRateMin = 1.5f;
+
+	private GameObject[] columns;
+	private Vector2 objectPoolPosition = new Vector2(-15f,-25f);
+	private float timeSinceLastSpawned;
+	private float spawnX = 10f;
+	private float spawnY = -3.7f;
+	private float nextSpawn;
+	private int currentColumn = 0;
+
+	void Start () 
+	{
+		columns = new GameObject[columnPoolSize];
+		for (int i = 0; i < columnPoolSize; i++) 
+		{
+			int indexPrefab = Random.Range (0, columnPrefab.Length - 1);
+			columns [i] = (GameObject)Instantiate(columnPrefab[indexPrefab], objectPoolPosition, Quaternion.identity);
+		}
+
+	}
+
+	void Update () 
+	{
+		timeSinceLastSpawned += Time.deltaTime;
+		if (timeSinceLastSpawned >= nextSpawn) 
+		{
+			columns [currentColumn].transform.position = new Vector2 (spawnX, spawnY);
+			currentColumn++;
+			if (currentColumn >= columnPoolSize) 
+				currentColumn = 0;
+		
+			NextTime ();
+
+		}
+
+	}
+
+	void NextTime() 
+	{
+		nextSpawn = Random.Range (spawnRateMin, spawnRateMax);
+		timeSinceLastSpawned = 0;
+	}
+}
